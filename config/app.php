d4966f5992d0ce9c580adcb735a58b5702c05570
hendriwhyu <hendriwp123@gmail.com>
2023-08-18 14:16:15 +0700

    feat: Config Spatie

--- config/app.php
+++ config/app.php
@@ -182,9 +182,11 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
