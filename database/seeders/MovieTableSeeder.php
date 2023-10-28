beb1c61f469ae65ddfa2307e6eed51066ff32af7
hendriwhyu <hendriwp123@gmail.com>
2023-10-28 22:45:01 +0700

    feat: migration and seeders file

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movies = [
+            [
+                'name' => 'The Pandemic: Corona',
+                'slug' => 'the-pandemic-corona',
+                'category' => 'Sci-fi',
+                'video_url' => 'https://youtu.be/uz9OwKHUcC0?si=ZGL3T_R2u7na7Hak',
+                'thumbnail' => 'https://shorturl.at/eorFX',
+                'rating' => 8.2,
+                'is_featured' => true
+            ],
+            [
+                'name' => 'The Godfather',
+                'slug' => 'the-godfather',
+                'category' => 'Drama',
+                'video_url' => 'https://youtu.be/UaVTIH8mujA?si=7_APOwOcn9YWEeUH',
+                'thumbnail' => 'https://shorturl.at/lswGY',
+                'rating' => 9.1,
+                'is_featured' => false
+            ],
+            [
+                'name' => 'The Godfather: Part II',
+                'slug' => 'the-godfather-part2',
+                'category' => 'Drama',
+                'video_url' => 'https://youtu.be/OA1ij0alE0w?si=D2arO_O2Nfb937Hj',
+                'thumbnail' => 'https://shorturl.at/nqVXY',
+                'rating' => 7.8,
+                'is_featured' => false
+            ],
+        ];
+
+        Movie::insert($movies);
+    }
+}
